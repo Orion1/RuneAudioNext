d07a960790c3ff40e9ba146421f577e2a31bf8ec
ACXgit <acx@ngi.it>
2014-03-25 22:54:20 +0100

    Fixato check window wisibility
    Ora il knob non rimane più indietro

--- app/templates/playback.php
+++ app/templates/playback.php
@@ -74,15 +74,15 @@
                 <div class="col-md-4 col-sm-6">
                     <div id="home-nas" class="home-block" data-path="NAS">
                         <i class="fa fa-sitemap"></i>
-                        <h3>Network mounts (2)</h3>
-                        2 items available
+                        <h3>Network mounts (1)</h3>
+                        1 item available
                     </div>
                 </div>
                 <div class="col-md-4 col-sm-6">
                     <div id="home-usb" class="home-block" data-path="USB">
                         <i class="fa fa-hdd-o"></i>
-                        <h3>USB storage (1)</h3>
-                        1 item available
+                        <h3>USB storage (0)</h3>
+                        no items available
                     </div>
                 </div>
 				<div class="col-md-4 col-sm-6">
--- assets/js/scripts-playback.js
+++ assets/js/scripts-playback.js
@@ -640,6 +640,49 @@ jQuery(document).ready(function($){ 'use strict';
 
 });
 
+// check visibility of the window
+(function() {
+    hidden = 'hidden';
+    // Standards:
+    if (hidden in document)
+        document.addEventListener('visibilitychange', onchange);
+    else if ((hidden = 'mozHidden') in document)
+        document.addEventListener('mozvisibilitychange', onchange);
+    else if ((hidden = "webkitHidden") in document)
+        document.addEventListener('webkitvisibilitychange', onchange);
+    else if ((hidden = "msHidden") in document)
+        document.addEventListener('msvisibilitychange', onchange);
+    // IE 9 and lower:
+    else if ('onfocusin' in document)
+        document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+        window.onpageshow = window.onpagehide
+            = window.onfocus = window.onblur = onchange;
+
+    function onchange (evt) {
+        var v = 'visible', h = 'hidden',
+            evtMap = {
+                focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+            };
+
+        evt = evt || window.event;
+        if (evt.type in evtMap) {
+            document.body.className = evtMap[evt.type];
+            //console.log('boh? = ', evtMap[evt.type]);
+        } else {
+            document.body.className = this[hidden] ? 'hidden' : 'visible';
+            if (this[hidden]) {
+                GUI.visibility = 'hidden';
+                //console.log('focus = hidden');
+            } else {
+                GUI.visibility = 'visible';
+                //console.log('focus = visible');
+            }
+        }
+    }
+})();
+
 // trigger social overlay
 (function() {
 	var triggerBttn = $('#social-overlay-open'),
