5134e3dd541327cab4fa711bbe50442dac80ad47
ACXgit <acx@ngi.it>
2014-04-18 01:33:03 +0200

    Testi network

--- app/templates/network.php
+++ app/templates/network.php
@@ -1,13 +1,14 @@
-<div class="container credits">
+<div class="container">
 	<h1>Network configuration</h1>
-	<form id="network-refresh" method="post">
-	<p><button class="btn btn-lg btn-primary" name="refresh" value="1" id="refresh"><i class="fa fa-refresh sx"></i>Refresh interfaces</button></p>
-	</form>
 	<div class="boxed">
-		<p>Bla bla bla</p>
+		<p>Configure wired and wireless connections. See below for the list of the active network interfaces as detected by the system.<br>
+		If your interface is connected but does not show, then try to refresh the list forcing the detect.</p>
+		<form id="network-refresh" method="post">
+			<button class="btn btn-lg btn-primary" name="refresh" value="1" id="refresh"><i class="fa fa-refresh sx"></i>Refresh interfaces</button>
+		</form>
 	</div>	
 	<h2>Network interfaces</h2>
-	<p>List of available network interfaces (click to configure)</p>
+	<p>List of active network interfaces. Click on an entry to configure the corresponding connection.</p>
 	<form id="network-interface-list" class="button-list" method="post">
 	<?php foreach ($this->nics as $key => $value): ?>
 		<p><a href="/network/edit/<?=$key ?>" class="btn btn-lg btn-default btn-block"> <i class="fa <?php if ($value->ip !== null): ?>fa-check green<?php else: ?>fa-times red<?php endif; ?> sx"></i> <strong><?=$key ?> </strong>&nbsp;&nbsp;&nbsp;&nbsp;<span><?php if ($value->ip !== null): ?><?=$value->ip ?><?php else: ?> --- UNCONFIGURED ---<?php endif; ?></span></a></p>
