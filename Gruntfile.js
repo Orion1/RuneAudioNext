5afb1748607cb5bff7793c3baa28560fe7c57a62
ACXgit <acx@ngi.it>
2014-04-18 14:50:13 +0200

    Configurazione Grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        'assets/js/vendor/*.js',
+        '!assets/js/runeui.min.js'
+      ]
+    },
+    recess: {
+      dist: {
+        options: {
+          compile: true,
+          compress: true
+        },
+        files: {
+          'assets/css/runeui.css': [
+            'assets/less/runeui.less'
+          ]
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/runeui.min.js': [
+            'assets/js/vendor/*.js',
+            'assets/js/*.js'
+          ]
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'assets/less/*.less',
+          'assets/less/bootstrap/*.less',
+          'assets/less/bootstrap-select/*.less',
+          'assets/less/csspinner/*.less',
+          'assets/less/themes/*.less',
+          'assets/less/toggle-switch/*.less',
+        ],
+        tasks: ['recess']
+      },
+      js: {
+        files: [
+          '<%= jshint.all %>'
+        ],
+        tasks: ['jshint', 'uglify']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'assets/css/runeui.css',
+          'assets/js/runeui.min.js',
+          'templates/*.php',
+          '*.php'
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/runeui.css',
+        'assets/js/runeui.min.js'
+      ]
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-recess');
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'recess',
+    'uglify'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
