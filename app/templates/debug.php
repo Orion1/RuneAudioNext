03870e63172f069e13b9065fb2f1ce772eff0f7e
ACXgit <acx@ngi.it>
2014-06-14 19:32:22 +0200

    Debug - copy to clipboard

--- app/templates/debug.php
+++ app/templates/debug.php
@@ -3,10 +3,10 @@
 	<div class="boxed">
 		<p>Below is displayed the raw output of RuneUI's debug section. It contains some important informations that could help to diagnosticate problems.<br> 
 		Please copy and paste it in your posts when asking for help <a href="http://www.runeaudio.com/forum/" title="RuneAudio Forum" target="_blank">in the forum</a>.</p>
-		<button id="copy-to-clipboard" class="btn btn-primary btn-lg" data-clipboard-target="debug-output" data-clipboard-text="Default clipboard text from attribute"><i class="fa fa-copy sx"></i> Copy data to clipboard</button>
+		<button id="copy-to-clipboard" class="btn btn-primary btn-lg" data-clipboard-target="clipboard_pre"><i class="fa fa-copy sx"></i> Copy data to clipboard</button>
 	</div>
 	<br>
-	<pre id="debug-output">
+	<pre id="clipboard_pre">
 		<?=$this->debug ?>
 	<pre>
 </div>
