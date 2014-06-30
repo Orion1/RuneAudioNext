87aecfb6acf469d6200872ef73997ceb34464a2e
Orion1 <degregori@gmail.com>
2014-06-30 08:10:13 +0000

    pulizia command/index.php

--- command/index.php
+++ command/index.php
@@ -36,108 +36,36 @@
 include($_SERVER['HOME'].'/app/config/config.php');
 // -- REWORK NEEDED --
 if (isset($_GET['cmd']) && $_GET['cmd'] != '') {
-        if ( !$mpd ) {
+
+    if ( !$mpd ) {
+	
         echo 'Error Connecting to MPD daemon ';
-        } else {
-            if ($_GET['cmd'] == 'enableao' && isset($_GET['ao'])) {
-                $outputs = _parseOutputsResponse(getMpdOutputs($mpd));
-                $cmdstr .= "pause \n";
-                $i = 0;
-                foreach ($outputs as $output) {
-                    if ($redis->get('hwplatformid') == '01' OR $redis->get('hwplatformid') == '03') {
-                        if ($i == 2 &&  ($_GET['ao'] == 2 OR $_GET['ao'] == 3)) {
-                            $cmdstr .= "enableoutput 2\n";
-                        } else if ($i == $_GET['ao']) {
-                            $cmdstr = "enableoutput ".$_GET['ao']."\n";
-                        } else {
-                            $cmdstr .= "disableoutput ".$i."\n";
-                        }
-                    }
-                $i++;
-                }
-                $cmdstr .= "play \n";
-                // disable all MPD outputs
-                // debug
-                runelog('MPD command (0):',$cmdstr);
-                sendMpdCommand($mpd,$cmdstr);
-                // Audio Output: raspberrypi
-                if ($redis->get('hwplatformid') == '01') {
-                    // AnalogJack
-                    if ($_GET['ao'] == 2) {
-                    $aosock = openMpdSocket('127.0.0.1', 13501);
-                    } else if ($_GET['ao'] == 3) {
-                    // HDMI
-                    $aosock = openMpdSocket('127.0.0.1', 13502);
-                    $cmdstr = 'enableoutput 2';
-                    }
-                    usleep(300000);
-                    sendMpdCommand($aosock,"\n");
-                    runelog('selected Rpi jack/hdmi internal switch:',$_GET['ao']);
-                    fclose($aosock);
-                }
-            // enable selected MPD output
-            // debug
-            runelog('MPD command (1):',$cmdstr);
-            sendMpdCommand($mpd,$cmdstr);
-            // debug
-            runelog('selected output interface:',$_GET['ao']);
-            playerSession('write',$db,'ao',$_GET['ao']);
-            } else if (($redis->get('hwplatformid') == '01') && ($redis->get('ao') == 2 OR $redis->get('ao') == 3) && ($_GET['cmd'] == 'next' OR $_GET['cmd'] == 'previous' OR preg_match('/play/', $_GET['cmd']) OR preg_match('/seek/', $_GET['cmd']))) {
-                // debug
-                runelog('MPD command (2):','pause');
-                sendMpdCommand($mpd,"pause");
-                // debug
-                if ($redis->get('state') == 'stop' && preg_match('/play/', $_GET['cmd'])) {
-                runelog('MPD command (3):','play');
-                sendMpdCommand($mpd,"play");
-                }
-                    // seek command fix
-                    if (preg_match('/seek/', $_GET['cmd'])) {
-                        runelog('--- [command/index.php] >>> OPEN MPD SOCKET --- (-1) ---','');
-                        $mpd2 = openMpdSocket(DAEMONIP, 6600);
-                        // debug
-                        runelog('MPD command (4):',$_GET['cmd']);
-                        sendMpdCommand($mpd2,$_GET['cmd']);
-                        // debug
-                        runelog('--- [command/index.php] --- CLOSE MPD SOCKET <<< (-1) ---','');
-                        closeMpdSocket($mpd2);
-                        $cmdstr = 'play';
-                    } else {
-                        $cmdstr = $_GET['cmd'];
-                    }
-                // close MPD socket and wait for 0.5 sec
-                // debug
-                runelog('--- [command/index.php] --- CLOSE MPD SOCKET <<< (0) ---','');
-                closeMpdSocket($mpd);
-                usleep(500000);
-                // debug
-                runelog('--- [command/index.php] >>> OPEN MPD SOCKET --- (0) ---','');
-                $mpd = openMpdSocket(DAEMONIP, 6600);
-                usleep(100000);
-                // debug
-                runelog('MPD command (5):',$cmdstr);
-                sendMpdCommand($mpd,$cmdstr);
-            } else {
-                // debug
-                runelog('MPD command (6):',$_GET['cmd']);
-                if ($_GET['cmd'] === 'renderui') {
-					ui_update($redis,$mpd);
-                } else if ($_GET['cmd'] === 'wifiscan') {
-					wrk_control($redis,'newjob', $data = array( 'wrkcmd' => 'wificfg', 'action' => 'scan' ));
-					echo 'wlan scan queued';
-					die;
-				} else {
-					sendMpdCommand($mpd,$_GET['cmd']);
-				}
-            }
+		
+    } else {
+           
+		// debug
+		runelog('MPD command: ',$_GET['cmd']);
+		if ($_GET['cmd'] === 'renderui') {
+			ui_update($redis,$mpd);
+		} else if ($_GET['cmd'] === 'wifiscan') {
+			wrk_control($redis,'newjob', $data = array( 'wrkcmd' => 'wificfg', 'action' => 'scan' ));
+			echo 'wlan scan queued';
+			die;
+		} else {
+			sendMpdCommand($mpd,$_GET['cmd']);
+		}
+            
         // debug
         runelog('--- [command/index.php] --- CLOSE MPD SOCKET <<< (1) ---','');
 		$response = readMpdResponse($mpd);
         closeMpdSocket($mpd);
 				echo $response;
-		}
+	}
+	
 } else {
-echo 'MPD COMMAND INTERFACE<br>';
-echo 'INTERNAL USE ONLY<br>';
-echo 'hosted on runeaudio.local:82';
+	
+	echo 'MPD COMMAND INTERFACE<br>';
+	echo 'INTERNAL USE ONLY<br>';
+	echo 'hosted on runeaudio.local:82';
+	
 }
