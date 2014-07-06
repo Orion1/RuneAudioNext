e734f320c603c739d9469ec8f9b80a7d600eb69b
ACXgit <acx@ngi.it>
2014-07-06 22:17:34 +0200

    Condizionale DEV per runeui.js

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -36,6 +36,6 @@
 <script src="<?=$this->asset('/js/vendor/bootstrap-select.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/parsley.min.js')?>"></script>
 <?php endif ?>
-<script src="<?=$this->asset('/js/runeui.min.js')?>"></script>
+<script src="<?=$this->asset('/js/runeui'. (($this->dev === '0') ? '.min' : '') .'.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/pnotify.custom.min.js')?>"></script>
 <script src="<?=$this->asset('/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js')?>"></script>
